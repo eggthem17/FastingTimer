c38fbdf681b71c5a20a2fe798c6552a3ef526f02
paxmiler <paxmiler@gmail.com>
2023-02-15 18:08:37 +0900

    Initial Commit

---
+++ FastingTimer_B/FastingTimer_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimer_BApp.swift
+//  FastingTimer_B
+//
+//  Created by Martin.Q on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimer_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
